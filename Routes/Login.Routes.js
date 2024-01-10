91d64c79b320fbdc775134ec59b121d88dd2af2d
kart2004 <karthikprakash999@gmail.com>
2024-01-10 21:33:37 +0530

    Demo

---
+++ Routes/Login.Routes.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path = require('path');
+
+const MONGO_URI = 'mongodb+srv://nithya3169:bcn8gMcHRRVqtW7E@clusteratms.ms3h1yl.mongodb.net/?retryWrites=true&w=majority'; 
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+    console.log('Connected to MongoDB database in Login');
+});
+
+
+
+router.get('/', (req, res) => {
+    res.render('login');
+    
+});
+
+
+// Define a route for the /verify endpoint
+router.post('/verify', async (req, res) => {
+    const { role, username, password } = req.body;
+
+    // Ensure that the db object is defined
+    if (!db) {
+        console.error('Internal Server Error: MongoDB connection not established w Login');
+        return res.status(500).send('Internal Server Error: MongoDB connection not established w Login');
+    }
+
+    try {
+        // Ensure that the db object has a 'collection' method before using it
+        if (typeof db.collection !== 'function') {
+            console.error('Internal Server Error: db.collection is not a function');
+            throw new Error('MongoDB connection error: db.collection is not a function');
+        }
+
+        // Perform authentication logic using your MongoDB connection
+        const usersCollection1 = db.collection('teacher');
+        const usersCollection2 = db.collection('student');
+        const teacher = await usersCollection1.findOne({ teacher_email: username, teacher_password: password });
+        const student = await usersCollection2.findOne({ student_email: username, student_password: password });
+        
+
+        if (teacher && role=="teacher") {
+            res.redirect('/login/teacher');
+        }
+        else if (student && role=="student") {
+            res.redirect('/login/student');
+        } else {
+            console.error('Authentication failed: Invalid username or password');
+            res.status(401).json({ message: 'Invalid username or password.' });
+        }
+    } 
+    catch (err) {
+        console.error('Error during authentication:', err);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+// Define a route for the /teacher endpoint
+router.get('/teacher', (req, res) => {
+    res.send('Welcome to the Teacher Dashboard');
+});
+router.get('/student', (req, res) => {
+    res.send('Welcome to the Student Dashboard');
+});
+
+
+module.exports = router;
---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const path = require('path');
+const app = express();
+const PORT = 4000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb+srv://nithya3169:bcn8gMcHRRVqtW7E@clusteratms.ms3h1yl.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('Mongodb connected app.js ...');
+    }).catch(error => {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    });
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+//Import and use the login route
+const loginRoutes = require('./Routes/Login.Routes');
+app.use('/login', loginRoutes);
+
+//Redirect to the login route
+app.get('/', (req, res) => {
+    console.log('Redirecting to /login');
+    res.redirect('/login');
+});
+
+app.get('/login/teacher', (req, res) => {
+    res.send('Welcome to the Teacher Dashboard');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on app http://localhost:${PORT}`);
+});
+
+
+
+

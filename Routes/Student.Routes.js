4c6c2077c86cd73a3dde726f6cf3c2d2399e7ff1
kart2004 <karthikprakash999@gmail.com>
2024-01-27 13:42:46 +0530

    FIN

--- Routes/Student.Routes.js
+++ Routes/Student.Routes.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bodyParser = require('body-parser');
-const path = require('path');
 const mongoose = require('mongoose');
-router.use(bodyParser.json());
-
 const MONGO_URI = 'mongodb+srv://nithya3169:bcn8gMcHRRVqtW7E@clusteratms.ms3h1yl.mongodb.net/?retryWrites=true&w=majority'; 
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -45,4 +41,31 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.post('/studentHistory/:student_id', async (req, res) => {
+    const studentId = req.params.student_id;
+    console.log('Received studentId: history', studentId);
+    if (!studentId) {
+        return res.status(400).json({ message: 'Student Id is required.' });
+    }
+    try {
+        const usersCollection1 = db.collection('student');
+        const student = await usersCollection1.findOne({ student_id: studentId });
+        if (!student) {
+            return res.status(404).json({ message: 'Student not found.' });
+        }
+        const className = student.student_class_name;
+        const usersCollection2 = db.collection('classes');
+
+        const selectedClass = await usersCollection2.findOne({ class_name: className });
+        if (!selectedClass) {
+            return res.status(404).json({ message: 'Class not found.' });
+        }
+        res.render('studentHistory', { student,
+            selectedClass
+         });
+    } catch (error) {
+        console.error('Error fetching class details:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
 module.exports = router;
